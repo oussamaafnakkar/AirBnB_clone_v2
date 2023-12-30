ad1d73c4313d2e8523a1f4abf7c4754c01e92038
oussamaafnakkar <oussamaafnakkar2002@gmail.com>
2023-12-30 18:37:28 +0000

    fix 7 file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,24 +3,25 @@
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
+    """Remove the current SQLAlchemy Session."""
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
-def states():
-    """List all states"""
-    states = storage.all("State")
-    # states_dic = {state['id']: state['name'] for state in states.values()}
-    return render_template('7-states_list.html',
-                           Table="States", states=states)
+def states_list():
+    """Display a HTML page with a list of all State objects sorted by name."""
+    states = storage.all("State").values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
+
