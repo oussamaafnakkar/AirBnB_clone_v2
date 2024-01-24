f33caa1e0353f2aaa1bc7613936398e2aef136fc
oussamaafnakkar <oussamaafnakkar2002@gmail.com>
2024-01-24 18:29:07 +0000

    flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,23 +1,12 @@
-#!/usr/bin/python3
-"""
-script that starts a Flask web application
-"""
-# importar la clase Flask q permite generar nuevas instancias de Flask
+# web_flask/0-hello_route.py
 from flask import Flask
-# crear una instancia de Flask, llamada app
+
 app = Flask(__name__)
-# usar un decorador de python, la función route recibe como parámetro la ruta
-# en donde queramos que se corra esta función hello
-# strict_slashes permite que cuando una ruta no tenga una barra (/) al final
-# la pueda redirigir correctamente
 
-
-@app.route('/', strict_slashes=False)
-# crear una función para imprimir Hello HBNB!
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello():
-    """This function returns Hello HBNB!"""
     return 'Hello HBNB!'
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000', debug=True)
