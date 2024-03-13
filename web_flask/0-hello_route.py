4aa0299edacec6772eef6f4cd7b704b78013deab
oussamaafnakkar <oussamaafnakkar2002@gmail.com>
2024-03-13 18:05:10 +0000

    alin

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,13 @@
-# web_flask/0-hello_route.py
+#!/usr/bin/python3
+""" This module starts a Flask web application """
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/airbnb-onepage/', strict_slashes=False)
-def hello():
+def hello_hbnb():
+    """ Returns a specific response """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
